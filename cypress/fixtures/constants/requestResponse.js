83639bf20c20470a8d4a07d953aa3a3e5f6198c6
Namra Anjum <namra.anjum1919@gmail.com>
2022-12-19 11:29:56 +0100

    added test for api

---
+++ cypress/fixtures/constants/requestResponse.js
@@ -0,0 +1,102 @@
+const name = 'Rock';
+const job = 'Teacher';
+const email = 'eve.holt@reqres.in';
+const password = 'abc';
+const updatedJob = 'Engineer'
+export const NEW_USER = {
+    name: name,
+    job: job
+};
+
+export const UPDATED_USER = {
+    name: name,
+    job: updatedJob
+};
+
+export const REGISTER_USER = {
+    email: email,
+    password: password
+};
+
+export const REGISTER_INVALID_USER = {
+    email: email,
+};
+
+export const ERROR_MESSAGES = {
+    MISSING_PASSWORD: 'Missing password'
+};
+
+
+export const USERS_SCHEMA = {
+    "$schema": "http://json-schema.org/draft-04/schema#",
+    "type": "object",
+    "properties": {
+        "page": {
+            "type": "integer"
+        },
+        "per_page": {
+            "type": "integer"
+        },
+        "total": {
+            "type": "integer"
+        },
+        "total_pages": {
+            "type": "integer"
+        },
+        "data": {
+            "type": "array",
+            "items": [
+                {
+                    "type": "object",
+                    "properties": {
+                        "id": {
+                            "type": "integer"
+                        },
+                        "email": {
+                            "type": "string"
+                        },
+                        "first_name": {
+                            "type": "string"
+                        },
+                        "last_name": {
+                            "type": "string"
+                        },
+                        "avatar": {
+                            "type": "string"
+                        }
+                    },
+                    "required": [
+                        "id",
+                        "email",
+                        "first_name",
+                        "last_name",
+                        "avatar"
+                    ]
+                }
+            ]
+        },
+        "support": {
+            "type": "object",
+            "properties": {
+                "url": {
+                    "type": "string"
+                },
+                "text": {
+                    "type": "string"
+                }
+            },
+            "required": [
+                "url",
+                "text"
+            ]
+        }
+    },
+    "required": [
+        "page",
+        "per_page",
+        "total",
+        "total_pages",
+        "data",
+        "support"
+    ]
+};
