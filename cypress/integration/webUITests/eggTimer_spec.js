e379c789b8c4a83c92809949f6756bafeac050b0
Namra Anjum <namra.anjum1919@gmail.com>
2022-12-20 00:55:05 +0100

    added tests for ui

--- cypress/integration/webUITests/eggTimer_spec.js
+++ cypress/integration/webUITests/eggTimer_spec.js
@@ -36,13 +36,44 @@ describe('egg timer functionality', () => {
         cy.get('.EggTimer-settings-close').click({ force: true })
     })
 
+    it('should verify if user can add label to the timer via url', () => {
+        cy.url().then(urlValue => cy.visit(urlValue + 'We will go live!/in/5minutes'))
+        cy.get('.ClassicTimer-label').should('have.text', 'We will go live!')
+    })
 
+    it('should verify the timer when user enters large integer values', () => {
+        cy.get('#EggTimer-start-time-input-text').type('57575775757575757575677575757')
+        cy.get('.validTime').click()
+        cy.contains('NaN')
+    })
 
+    it('should verify if the user is able to toggle the toolbar', () => {
+        cy.contains('/Morning').click()
+        cy.url().should('be.equal', 'https://e.ggtimer.com/morning')
+        cy.get('.EggTimer-timer-bar-toggle-collapse').click()
+    })
 
+    it('should verify if the user is able to perform actions on toolbar', () => {
+        cy.url().then(urlValue => cy.visit(urlValue + '120'))
+        cy.get('#theme-select').eq(0).select('Dot Matrix').should('have.value', 'gg_timer_dotmatrix')
+        cy.get('.UI-toggle-switch-off').eq(1).click({ force: true })
+        cy.get('.EggTimer-timer-bar-help').click()
+        cy.get('.EggTimer-settings-close').click({ force: true })
+        cy.get('[title="Restart timer"]').click()
+        cy.get('.EggTimer-timer-bar-back').click()
+    })
 
-
-
-
+    it.only('should verify the alert when time expires', () => {
+        cy.get('#EggTimer-start-time-input-text').type('10')
+        cy.get('.validTime').click()
+        //enable notification from toolbar
+        cy.get('.UI-toggle-switch-on.selected').eq(0).click({ foce: true })
+        cy.wait(10000)
+        //verify alert
+        cy.on('window:alert', (txt) => {
+            expect(txt).to.contains('Time Expired!');
+        })
+    })
 
 })
 
